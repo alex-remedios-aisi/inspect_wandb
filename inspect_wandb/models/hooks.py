7ba3b6deae4e323de5f16f2dddf7d549db87b910
Daniel Polatajko <danielpolatajko@gmail.com>
2025-08-31 17:48:10 +0100

    feat: configure models tags via settings (#103)
    * add tags to settings
    
    * update pre commit
    
    * mypy
    
    * uv lock

--- inspect_wandb/models/hooks.py
+++ inspect_wandb/models/hooks.py
@@ -106,6 +106,9 @@ class WandBModelHooks(Hooks):
         else:
             self.run.tags = inspect_tags
 
+        if self.settings.tags is not None and self.run.tags is not None:
+            self.run.tags = self.run.tags + tuple(self.settings.tags)
+
     
     @override
     async def on_task_end(self, data: TaskEnd) -> None:
